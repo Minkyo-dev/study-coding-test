46f3de1d85a2ad59081bb64bedb0633971a393b2
Minkyo <kyodev211101@gmail.com>
2026-01-03 16:46:52 -0500

    백준 2003

--- problems/baekjoon/2003/_test.py
+++ problems/baekjoon/2003/_test.py
@@ -1,14 +1,16 @@
 import pytest
 from solution import mysolution, othersolution
 
-args = "expected"
-params = []
+args = "N, M, seq, expected"
+params = [
+    pytest.param(4, 2, "1 1 1 1", 3, id="sinario1"),
+    pytest.param(10, 5, "1 2 3 4 2 5 3 1 1 2", 3, id="sinario2"),
+]
 
 @pytest.mark.parametrize(args, params)
-def test_mysolution(expected):
-    assert mysolution() == expected
+def test_mysolution(N, M, seq,expected):
+    assert mysolution(N, M, seq) == expected
 
-@pytest.mark.parametrize(args, params)
-def test_othersolution(expected):
-    assert othersolution() == expected
-    
+# @pytest.mark.parametrize(args, params)
+# def test_othersolution(N, M, seq, expected):
+#     assert othersolution(N, M, seq) == expected
--- problems/baekjoon/2003/solution.py
+++ problems/baekjoon/2003/solution.py
@@ -7,14 +7,58 @@ sys.path.append(BASE_DIR)
 from utils.profiling import profile_time_memory
 
 @profile_time_memory
-def mysolution():
-    pass
+def mysolution(N:int, M:int, seq:str):
+    """
+    args:
+      N : 수열의 개수
+      M : 합의 결과
+      seq: 수열 문자열 ex. '1 1 1 1'
+    """
+    seq_ls = list(map(int, seq.split()))
+    case_cnt = 0
+
+    for i in range(N):
+        r_idx = i
+        case_sum = 0
+        if seq_ls[i] == M:
+            case_cnt += 1
+            print("처음부터 완료", i, r_idx)
+            continue
+        else:
+            while r_idx <= N:
+                if case_sum == M:
+                    case_cnt += 1
+                    print("while 완료", i, r_idx)
+                    break
+                elif case_sum > M:
+                    print("while 커짐", i, r_idx)
+                    break
+                case_sum += seq_ls[r_idx]
+                print("while 인덱스 추가", case_sum, i, r_idx)
+                r_idx += 1
+    return case_cnt
+
 
 @profile_time_memory
-def othersolution():
+def othersolution(N, M, seq):
+    start =0
+    end = 0
+    sum = seq[0]
+
+    sount = 0
+    while True:
+        # 현재 구간 합이 M인지 확인
+        if sum == M:
+            count += 1
+        # 구간 업데이트
+        if (sum>= M):
+            start+= 1
+            sum -= A[start-1]
     pass
 
 if __name__ == "__main__":
-    mysolution()
-    othersolution()
-    
+    N = 4
+    M = 2
+    seq = "1 1 1 1"
+    print(mysolution(N, M, seq))
+    # othersolution()
